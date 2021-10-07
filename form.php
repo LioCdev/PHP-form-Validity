97fb7c5f8c64eabde06ba2e3c53b26d4f8d80d01
LioCdev <lionel.choppin@gmail.com>
2021-10-07 15:45:17 +0200

    fist commit

---
+++ form.php
@@ -0,0 +1,28 @@
+<form  action="/thanks.php"  method="post">
+    <div>
+      <label  for="nom">Nom :</label>
+      <input  type="text"  required="required" id="nom"  name="user_name">
+    </div>
+    <div>
+      <label  for="courriel">Courriel :</label>
+        <input  type="email"  required="required" id="courriel"  name="user_email">
+    </div>
+    <div>
+        <label for="phone">Téléphone :</label>
+        <input type="number" required="required" id="telephone" name="user_phone">   
+    </div>
+    <div>
+        <label for="sujet">Choisir un sujet :</label>
+        <select id="sujet" name="sujet">
+            <option value="PHP">PHP</option>
+            <option value="JS">JS</option>
+        </select>
+    </div>
+    <div>
+      <label  for="message">Message :</label>
+      <textarea  required="required" id="message"  name="user_message"></textarea>
+    </div>
+    <div>
+    <input type="submit" value="Envoyer votre message" />
+    </div>
+  </form>
---
+++ thanks.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>thank you</title>
+</head>
+<body>
+
+<?php
+
+$nameErr = $phoneErr = $emailErr = $messageErr = $emailNotValid = "";
+
+$name = $_POST["user_name"];
+$email = $_POST["user_email"];
+$phone = $_POST["user_phone"];
+$message = $_POST["user_message"];
+$sujet = $_POST['sujet'];
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($name)) {
+            $nameErr = "name is required";
+            echo $nameErr;  
+        } elseif (empty($email)) {
+            $emailErr = "email is required";
+            echo $emailErr;
+        } elseif (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailNotValid = "email is not valid";
+            echo $emailNotValid;
+        } elseif (empty($phone)) {
+            $phoneErr = "phone is required";
+            echo $phoneErr;          
+        } elseif (empty($message)) {
+            $messageErr = "message is required";
+            echo $messageErr;
+    } else {
+        echo 'Merci '.$name.' de nous avoir contacté à propos de '.$sujet.'.'.'<br>';
+        echo 'Un de nos conseiller vous contactera soit à l\'adresse '.$email.' ou par telephone au '.$phone.'.'.'<br>';
+    }
+}
+
+
+?>
+<!-- <p>Merci <?= $name; ?> de nous avoir contacté à propos de <?= $_POST['sujet']; ?>.
+Un de nos conseiller vous contactera soit à l’adresse <?= $email; ?> ou par téléphone au <?= $phone; ?> dans les plus brefs délais pour traiter votre demande : 
+<?= $message; ?></p>
+         -->
+
+</body>
+
+</html>
